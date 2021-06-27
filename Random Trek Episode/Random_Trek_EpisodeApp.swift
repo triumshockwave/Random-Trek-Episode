28a9cf84ac3fc0132f1103c759492093072a95f9
Brent Amersbach <triumshockwave@gmail.com>
2021-06-27 05:08:04 -0400

    Initial Commit

---
+++ Random Trek Episode/Random_Trek_EpisodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Trek_EpisodeApp.swift
+//  Random Trek Episode
+//
+//  Created by Brent Amersbach on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Random_Trek_EpisodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
