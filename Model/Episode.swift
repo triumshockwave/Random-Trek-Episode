c96e143cfd79437c73997a7848f7f81261a2b754
Brent Amersbach <triumshockwave@gmail.com>
2021-06-27 05:12:19 -0400

    Added Episode and Series structs

---
+++ Model/Episode.swift
@@ -0,0 +1,15 @@
+//
+//  Episode.swift
+//  Random Trek Episode
+//
+//  Created by Brent Amersbach on 6/27/21.
+//
+
+import Foundation
+
+struct Episode {
+    var title: String
+    var series: String
+    var season: Int
+    var number: Int
+}
---
+++ Model/Series.swift
@@ -0,0 +1,13 @@
+//
+//  Series.swift
+//  Random Trek Episode
+//
+//  Created by Brent Amersbach on 6/27/21.
+//
+
+import Foundation
+
+struct Series {
+    var name: String
+    var seasons: Int
+}
