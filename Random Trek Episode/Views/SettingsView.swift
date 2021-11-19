3fb17d83f61be5efc0b6a33058aaf8bdb9359a31
Brent Amersbach <triumshockwave@gmail.com>
2021-11-18 21:58:27 -0500

    Creation of toggles using loop working

--- Random Trek Episode/Views/SettingsView.swift
+++ Random Trek Episode/Views/SettingsView.swift
@@ -17,11 +17,11 @@ struct SettingsView: View {
     //MARK: - App Logic Methods
     
     func toggleAllOn() {
-        
+
     }
     
     func toggleAllOff() {
-        
+
     }
     
     //MARK: - View hierarchy
@@ -32,33 +32,12 @@ struct SettingsView: View {
                 Form() {
                     Section(header: Text("Series Selection")) {
                         
-                        Toggle(isOn: $applicationOptions.seriesList[0].isSelected) {
-                            Text(applicationOptions.seriesList[0].abbreviation)
+                        ForEach(applicationOptions.seriesList.indices) { i in
+                            Toggle(isOn: $applicationOptions.seriesList[i].isSelected) {
+                                Text(applicationOptions.seriesList[i].abbreviation)
+                            }
                         }
-                        Toggle(isOn: $applicationOptions.seriesList[1].isSelected) {
-                            Text(applicationOptions.seriesList[1].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[2].isSelected) {
-                            Text(applicationOptions.seriesList[2].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[3].isSelected) {
-                            Text(applicationOptions.seriesList[3].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[4].isSelected) {
-                            Text(applicationOptions.seriesList[4].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[5].isSelected) {
-                            Text(applicationOptions.seriesList[5].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[6].isSelected) {
-                            Text(applicationOptions.seriesList[6].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[7].isSelected) {
-                            Text(applicationOptions.seriesList[7].abbreviation)
-                        }
-                        Toggle(isOn: $applicationOptions.seriesList[8].isSelected) {
-                            Text(applicationOptions.seriesList[8].abbreviation)
-                        }
+                        
                     }
                     Section(header: Text("Toggle All")) {
                         HStack() {
