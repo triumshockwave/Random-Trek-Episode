8c5a7506ce9a1f0f86ca7a85b129685e2c7e253e
Brent Amersbach <triumshockwave@gmail.com>
2021-11-18 20:49:49 -0500

    Creation of Series instances in Options moved to init()

--- Random Trek Episode/Model/Options.swift
+++ Random Trek Episode/Model/Options.swift
@@ -19,15 +19,17 @@ class Options: ObservableObject {
     @Published var picSelected = true
     @Published var ldSelected  = true
     
-    @Published var seriesList: [Series] = [
-        Series(seriesTitle: "The Original Series", abbreviation: "TOS", isSelected: true, episodes: []),
-        Series(seriesTitle: "The Animated Series", abbreviation: "TAS", isSelected: true, episodes: []),
-        Series(seriesTitle: "The Next Generation", abbreviation: "TNG", isSelected: true, episodes: []),
-        Series(seriesTitle: "Deep Space Nine", abbreviation: "DS9", isSelected: true, episodes: []),
-        Series(seriesTitle: "Voyager", abbreviation: "VOY", isSelected: true, episodes: []),
-        Series(seriesTitle: "Enterprise", abbreviation: "ENT", isSelected: true, episodes: []),
-        Series(seriesTitle: "Discovery", abbreviation: "DSC", isSelected: true, episodes: []),
-        Series(seriesTitle: "Picard", abbreviation: "PIC", isSelected: true, episodes: []),
-        Series(seriesTitle: "Lower Decks", abbreviation: "LD", isSelected: true, episodes: [])
-    ]
+    @Published var seriesList: [Series] = []
+    
+    init() {
+        self.seriesList.append(Series(seriesTitle: "The Original Series", abbreviation: "TOS", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "The Animated Series", abbreviation: "TAS", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "The Next Generation", abbreviation: "TNG", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "Deep Space Nine", abbreviation: "DS9", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "Voyager", abbreviation: "VOY", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "Enterprise", abbreviation: "ENT", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "Discovery", abbreviation: "DSC", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "Picard", abbreviation: "PIC", isSelected: true, episodes: []))
+        self.seriesList.append(Series(seriesTitle: "Lower Decks", abbreviation: "LD", isSelected: true, episodes: []))
+    }
 }
